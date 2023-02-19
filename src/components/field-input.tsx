e610515420a8f213f4226edc32db13f44d49778b
samcyn <samsoniyanda@outlook.com>
2023-02-19 15:28:19 +0000

    style: format code with prettier

--- src/components/field-input.tsx
+++ src/components/field-input.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FieldInput = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
-  return (<input {...props} />)
-}
+	return <input {...props} />;
+};
 
 export default FieldInput;
--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -4,32 +4,35 @@ import type { Todo } from "../app.types";
 import FieldInput from "./field-input";
 
 type TodoProps = {
-  todo: Todo,
-  onToggle: (todo: Todo) => void
+	todo: Todo;
+	onToggle: (todo: Todo) => void;
 };
 
 export function TodoItem({ onToggle, todo }: TodoProps) {
 	const { title, tags = [], completed, id } = todo;
 
-  const idString = id.toString();
+	const idString = id.toString();
 
-  const handleToggle = () => {
-    onToggle(todo);
-  }
+	const handleToggle = () => {
+		onToggle(todo);
+	};
 
 	return (
-		<li className={`todo ${completed ? 'completed' : ''}`}>
+		<li className={`todo ${completed ? "completed" : ""}`}>
 			<div>
-				<FieldInput type="checkbox" checked={completed} id={idString} onChange={handleToggle} />
-				<label htmlFor={idString}>
-          {title}
-				</label>
+				<FieldInput
+					type="checkbox"
+					checked={completed}
+					id={idString}
+					onChange={handleToggle}
+				/>
+				<label htmlFor={idString}>{title}</label>
 			</div>
-      <ul>
-        {
-          tags.map(tag => (<li key={tag}>{tag}</li>))
-        }
-      </ul>
+			<ul>
+				{tags.map((tag) => (
+					<li key={tag}>{tag}</li>
+				))}
+			</ul>
 		</li>
 	);
 }
